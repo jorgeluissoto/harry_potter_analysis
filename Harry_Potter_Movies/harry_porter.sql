11c5dfe0b248bbf28ffbd51def536f671ad669f1
Jorge Soto <100888747+jorgeluissoto@users.noreply.github.com>
2023-04-20 18:40:07 -0400

    Add files via upload

---
+++ Harry_Potter_Movies/harry_porter.sql
@@ -0,0 +1,15 @@
+-- Recommended Analysis
+
+-- Which movie has the most lines of dialog? Is it also the longest movie?
+
+-- How have the characters with the most lines of dialogue varied across movies?
+
+-- What is the most popular location in each film?
+
+-- Which spells are used the most across the franchise? Do characters have a favorite spell?
+
+
+
+
+
+
